5151116191f6c7ced113f955c531d5003001e313
ryan <ryan.cotsakis@polymtl.ca>
2020-05-03 16:01:01 -0700

    shifts the line in pingpong

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Hockey Betting
 
 - Uses Nadaraya-Watson estimator with triangular kernels to predict the home team's score, and the visitor's score of a hockey game
-- The space in which the points live are more spread out in the direction most correlated to the score
+- Points in feature space are more spread out in the direction most correlated to the score
 - Highly correlated features are amalgamated into a single dimension in feature space, effectively shrinking the dimension
-- The goals arrive according to a Poisson process with its mean distributed to a lognormal distribution with mean predicted by the Nadaraya-Watson estimator
+- The goals arrive according to a Poisson process with its mean predicted by the Nadaraya-Watson estimator
 - Able to assign a probability to various outcomes of the game's score
--- src/pingpong.py
+++ src/pingpong.py
@@ -1,8 +1,10 @@
 from scipy.misc import comb
+import numpy as np
+import matplotlib.pyplot as plt
 import os
 
 # Return the probability that player 1 wins the set
-def who_wins(score1, score2, playto):
+def who_wins(score1, score2, playto, p=0.5):
 	if score1 == playto:
 		return 1.0
 	prob = 0
@@ -10,18 +12,18 @@ def who_wins(score1, score2, playto):
 	largest = k + (playto - 1 - score2)
 
 	for n in range(k, largest + 1):
-		prob += comb(n,k)*0.5**(n+1)
+		prob += comb(n,k) * p**(k+1) * (1-p)**(n-k)
 
 	return prob
 
 
 # Return the probability that player 1 wins the match given the current score, and the number of sets won
-def win_match(score1, score2, sets1, sets2, match_playto=3, set_playto=11):
+def win_match(score1, score2, sets1, sets2, p=0.5, match_playto=3, set_playto=11):
 	if sets1 == match_playto:
 		return 1.0
 	prob = 0
-	prob += who_wins(sets1 + 1, sets2, match_playto) * who_wins(score1, score2, set_playto)
-	prob += who_wins(sets1, sets2 + 1, match_playto) * who_wins(score2, score1, set_playto)
+	prob += who_wins(sets1 + 1, sets2, match_playto, who_wins(0, 0, set_playto, p)) * who_wins(score1, score2, set_playto, p)
+	prob += who_wins(sets1, sets2 + 1, match_playto, who_wins(0, 0, set_playto, p)) * who_wins(score2, score1, set_playto, 1-p)
 	return prob
 
 
@@ -47,25 +49,30 @@ def print_odds(prob, maxi=100, bank=50):
 	print("		Bet the above amount times 10*(line-odds)")
 
 
-def newgame():
+
+def newgame(odds=None):
 	os.system('clear')
 	score1 = 0
 	score2 = 0
 	sets1 = 0
 	sets2 = 0
+	if odds is not None:
+		p = (1./odds[0] - 1./odds[1] + 3.8) / 7.6
+	else:
+		p = 0.5
 
 	while True:
-
+		print("Line start: {}".format(odds))
 		print("Match Score: {}:{}".format(sets1,sets2))
 		print("Set Score: {}:{}".format(score1,score2))
 		print()
 
 		print("Odds for the match:")
-		print_odds(win_match(score1, score2, sets1, sets2))
+		print_odds(win_match(score1, score2, sets1, sets2, p=p))
 		print()
 
 		print("Odds for this set:")
-		print_odds(who_wins(score1, score2, 11))
+		print_odds(who_wins(score1, score2, 11, p=p))
 		print()
 
 		query = input('>> ')
@@ -91,5 +98,9 @@ def newgame():
 				sets2 = 0
 			elif c == 'q':
 				return
+			elif c == ',':
+				newgame([float(o) for o in query.split(',')])
+				return
+
 
 newgame()
